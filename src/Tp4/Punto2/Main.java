31cf360b88134b0ffe68438c7d2f2f346bad0730
Romangonzalezz <romanynaarut@gmail.com>
2023-05-28 10:25:31 -0300

    metodo generar pasword segun longitud

--- src/Tp4/Punto2/Main.java
+++ src/Tp4/Punto2/Main.java
@@ -1,2 +1,21 @@
-package Tp4.Punto2;public class Main {
+package Tp4.Punto2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Password password = new Password();
+        password.setContrasenia("PruAbSFrueba");
+        password.esFuerte(password.getContrasenia());
+
+        Password password2 = new Password();
+        password2.setContrasenia("PruAbruEba123456");
+        password2.esFuerte(password2.getContrasenia());
+
+        Password password3 = new Password();
+        password3.generarPassword(8);
+        password3.generarPassword(3);
+        password3.generarPassword(10);
+
+
+    }
 }
--- src/Tp4/Punto2/Password.java
+++ src/Tp4/Punto2/Password.java
@@ -1,2 +1,69 @@
-package Tp4.Punto2;public class Password {
+package Tp4.Punto2;
+import java.util.Random;
+
+
+public class Password {
+    private String contrasenia;
+    private int longitud = 8;
+
+    Random random = new Random();
+    public Password() {
+    }
+
+    public Password(int longitud) {
+        this.longitud = random.nextInt(longitud);
+    }
+
+    public boolean esFuerte(String contrasenia) {
+        int contador_mayus = 0;
+        int contador_minus = 0;
+        int contador_numeros = 0;
+
+        for (int i = 0; i < contrasenia.length(); i++) {
+            char c = contrasenia.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                contador_mayus++;
+            } else if (Character.isLowerCase(c)) {
+                contador_minus++;
+            } else if (Character.isDigit(c)) {
+                contador_numeros++;
+            }
+        }
+
+        System.out.println("Mayúsculas: " + contador_mayus);
+        System.out.println("Minúsculas: " + contador_minus);
+        System.out.println("Números: " + contador_numeros);
+
+        if (contador_mayus > 2 && contador_minus > 1 && contador_numeros > 5) {
+            System.out.println("Su contraseña es lo suficientemente fuerte. ¡Bien hecho!");
+            System.out.println();
+            return true;
+        } else {
+            System.out.println("Su contraseña es demasiado débil. ¡Inténtelo de nuevo!");
+            System.out.println();
+            return false;
+        }
+    }
+
+    public String generarPassword(int longitud){
+        Random randomPass = new Random();
+        String pass = "";
+        for(int i = 0; i < longitud; i++){
+
+            int randomIndex = randomPass.nextInt(longitud);
+            pass += randomIndex;
+        }
+        System.out.println();
+        System.out.println("Su contrasenia aleatoria es: " + pass);
+        return pass;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
 }
